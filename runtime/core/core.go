3af460b6942b21e0f91e7ae3409f16877c8816d9
ReeseHatfield <hatfield.69@wright.edu>
2024-10-23 20:53:05 -0400

    add ipc from key

--- runtime/core/core.go
+++ runtime/core/core.go
@@ -1,9 +1,12 @@
 package core
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"os/exec"
+	"strings"
 )
 
 type Core struct {
@@ -34,14 +37,44 @@ func (c *Core) Run() {
 
 func (c *Core) run() {
 
-	cmd := exec.Command("/bin/sh", "-c", "cd darkleaf-gui && npm run start --loglevel=verbose")
+	keyArg := fmt.Sprintf("--key=%s", c.key)
 
-	cmdOutput, err := cmd.CombinedOutput()
+	cmd := exec.Command("/bin/sh", "-c", "cd darkleaf-gui && npm run start --loglevel=verbose -- "+keyArg)
 
+	stdout, err := cmd.StdoutPipe()
 	if err != nil {
-		log.Fatalf("Error running command: %v\nOutput: %s", err, string(cmdOutput))
+		log.Fatalf("Error getting stdout pipe: %v", err)
+	}
+
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		log.Fatalf("Error getting stderr pipe: %v", err)
+	}
+
+	if err := cmd.Start(); err != nil {
+		log.Fatalf("Error starting command: %v", err)
+	}
+
+	streamOutput := func(pipe io.ReadCloser, pipeName string) {
+		scanner := bufio.NewScanner(pipe)
+		for scanner.Scan() {
+			line := scanner.Text()
+			fmt.Printf("[%s] %s\n", pipeName, line)
+			if strings.Contains(line, "IPC Message:") {
+				fmt.Println("Received message from Electron:", strings.TrimPrefix(line, "IPC Message:"))
+			}
+		}
+		if err := scanner.Err(); err != nil {
+			log.Fatalf("Error reading from %s: %v", pipeName, err)
+		}
+	}
+
+	go streamOutput(stdout, "stdout")
+	go streamOutput(stderr, "stderr")
+
+	if err := cmd.Wait(); err != nil {
+		log.Fatalf("Command finished with error: %v", err)
 	}
 
 	fmt.Println("Finished?")
-	fmt.Println(string(cmdOutput))
 }
